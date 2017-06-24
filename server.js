2eaad5a31a12dcbb82ac106a6bb647df8e40acbd
Winston Lum <meow@Winstons-MacBook-Pro.local>
2017-06-24 11:16:54 -0400

    server changes

--- server.js
+++ server.js
@@ -9,30 +9,38 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
 app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 
-var reservations = [];
+var reservations = [{
+	customerName: "Brian",
+	phoneNumber: "9085106980",
+	customerEmail: "kara",
+	customerID: "12"
+}];
 var waitingList = [];
 
 app.get("/", function(req, res) {
-  res.sendFile(path.join(__dirname, "index.html"));
+  	res.sendFile(path.join(__dirname, "index.html"));
 });
 
 app.get("/reservations", function(req, res) {
-  res.sendFile(path.join(__dirname, "reserve.html"));
+  	res.sendFile(path.join(__dirname, "reserve.html"));
 });
 
 app.get("/waiting-list", function(req, res){
-	res.sendFile(path.join(__dirname, "waiting-list.html"));
+	res.sendFile(path.join(__dirname, "tables.html"));
 })
 
-
 app.post("/api/new", function(req, res) {
-  var newReservation = req.body;
-  var reservation = newReservation.name;
+  	var newReservation = req.body;
 
-  reservations.push(reservation, newReservation);
+  	if(reservations.length < 6){
+  		reservations.push(newReservation);
+  	}
+  	else {
+  		waiting-list.push(newReservation);
+  	}
 });
 
 app.listen(PORT, function() {
-  console.log("App listening on PORT " + PORT);
+  	console.log("App listening on PORT " + PORT);
 });
 
